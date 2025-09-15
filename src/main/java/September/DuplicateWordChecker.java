6ecb989fe3e119984e2c8c4d58c266329d448ebc
masumrazait <masumrazait@gmail.com>
2025-09-15 22:08:40 +0530

    added new code

--- src/main/java/September/DuplicateWordChecker.java
+++ src/main/java/September/DuplicateWordChecker.java
@@ -1,21 +1,27 @@
 package September;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class DuplicateWordChecker {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String input = "Tata Audi Tata Maruti Tata Dell Hp Hp";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the sentence from input: ");
+		String input = sc.nextLine();
 		String[] word = input.split(" ");
+
 		Set<String> uniqWord = new HashSet<>();
 		for (String words : word) {
 			uniqWord.add(words.trim());
+
 		}
 		for (String words : uniqWord) {
 			System.out.println(words);
+
 		}
+
 	}
 
 }
