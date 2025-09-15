e60631edf4ca14edfdbc41b0ecf7b0a77801c541
masumrazait <masumrazait@gmail.com>
2025-09-15 21:17:54 +0530

    addded more code

---
+++ src/main/java/September/CheckNumberIsLeapYear.java
@@ -0,0 +1,20 @@
+package September;
+
+import java.util.Scanner;
+
+public class CheckNumberIsLeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Input = new Scanner(System.in);
+		System.out.println("enter the year: ");
+		int year = Input.nextInt();
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 4 == 0)) {
+			System.out.println(year + " year is leap year");
+		} else {
+			System.out.println(year + " year is not a leap year");
+		}
+
+	}
+
+}
---
+++ src/main/java/September/EvenOdd.java
@@ -0,0 +1,20 @@
+package September;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Input = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num = Input.nextInt();
+		if (num % 2 == 0) {
+			System.out.println(num + " this is even number ");
+		} else {
+			System.out.println(num + " this is odd number");
+		}
+
+	}
+
+}
