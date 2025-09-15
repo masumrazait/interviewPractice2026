ddbf177f270f053c6d0247c58b0741db2bcfb447
masumrazait <masumrazait@gmail.com>
2025-09-15 18:27:46 +0530

    modification in palindrome

--- src/main/java/September/Palindrom.java
+++ src/main/java/September/Palindrom.java
@@ -1,25 +1,24 @@
 package September;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Palindrom {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the word: ");
-		String rev = "";
 		String word = sc.nextLine();
+		String rev = "";
 		String orignal = word;
 		int len = word.length();
 		for (int i = len - 1; i >= 0; i--) {
 			rev = rev + word.charAt(i);
 		}
 		if (orignal.equalsIgnoreCase(rev)) {
-			System.out.println("sring is palindromm: " + orignal);
+			System.out.println("String is palindrom: " + orignal);
 		} else {
-			System.out.println("string is not palindrom: " + rev);
+			System.out.println("String is not palindrom: " + rev);
 		}
-
 	}
 
 }
