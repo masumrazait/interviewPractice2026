a0c4e4fc918e0ddfaac01c44493aca2bd701531e
masumrazait <masumrazait@gmail.com>
2025-09-15 16:59:08 +0530

    java practices code

---
+++ src/main/java/September/ReverseString.java
@@ -0,0 +1,15 @@
+package September;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter you name: ");
+		String Name = sc.nextLine();
+		System.out.println("Hello " + Name);
+
+	}
+
+}
