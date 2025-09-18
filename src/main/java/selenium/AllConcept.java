d5d51619b78e84fd12c0e86156e8c22c57ae72f8
masumrazait <masumrazait@gmail.com>
2025-09-18 20:49:47 +0530

    added selenium code

---
+++ src/main/java/selenium/AllConcept.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AllConcept {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://masumrazait.netlify.app/");
+		driver.getPageSource();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+}
