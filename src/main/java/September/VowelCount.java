b87ab7a8f0cda3a19064a60a93b7e3eb2b9b567d
masumrazait <masumrazait@gmail.com>
2025-09-21 00:34:18 +0530

    added

---
+++ src/main/java/September/VowelCount.java
@@ -0,0 +1,19 @@
+package September;
+
+public class VowelCount {
+	public static void main(String[] args) {
+
+		String str = "masumraza";
+		String vowel = "aeiouAEIOU";
+		int count = 0;
+		for (char c : str.toCharArray()) {
+			if (vowel.indexOf(c) != -1) {
+				System.out.println(c);
+				count++;
+
+			}
+		}
+		System.out.println(count);
+	}
+
+}
