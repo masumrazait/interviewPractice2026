6d4dfdedc2c7b25c377e1d41eaed0b61a899d646
masumrazait <masumrazait@gmail.com>
2025-09-21 15:55:44 +0530

    added new code

---
+++ src/main/java/Sept21/ExelReader.java
@@ -0,0 +1,20 @@
+package Sept21;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExelReader {
+
+	public static void main(String[] args) throws IOException {
+		String str = "./TestData/testdata.xlsx";
+		FileInputStream fis=new FileInputStream(str);
+		XSSFWorkbook workbook=new XSSFWorkbook(fis);
+		XSSFSheet sheet=workbook.getSheetAt(0);
+		String cellvalue=sheet.getRow(1).getCell(0).getStringCellValue();
+		System.out.println(cellvalue);
+	}
+
+}
