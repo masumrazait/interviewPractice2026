fb639acb4ab6087b5acd4b8ad7a5e92f27e88438
masumrazait <masumrazait@gmail.com>
2025-09-23 22:07:46 +0530

    added new code

---
+++ src/main/java/JavaInterview_Code/FibonnacciSeries.java
@@ -0,0 +1,18 @@
+package JavaInterview_Code;
+
+public class FibonnacciSeries {
+
+	public static void main(String[] args) {
+		int n1 = 0, n2 = 1, sum;
+		int num = 18;
+		System.out.println(n1 + " " + n2);
+		for (int i = 3; i <= num; i++) {
+			sum = n1 + n2;
+			n1 = n2;
+			n2 = sum;
+			System.out.println(sum);
+		}
+
+	}
+
+}
