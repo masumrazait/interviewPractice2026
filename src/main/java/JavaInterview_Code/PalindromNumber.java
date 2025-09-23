3154333e5398df1da5dba3c2ec2c63ca368ac3c5
masumrazait <masumrazait@gmail.com>
2025-09-23 22:31:22 +0530

    added new packages

---
+++ src/main/java/JavaInterview_Code/PalindromNumber.java
@@ -0,0 +1,23 @@
+package JavaInterview_Code;
+
+import java.util.Scanner;
+
+public class PalindromNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your the number: ");
+		int num = sc.nextInt();
+		int rev = 0;
+		int original = num;
+		while (num != 0) {
+			rev = rev * 10 + num % 10;
+			num = num / 10;
+		}
+		if (original == rev) {
+			System.out.println("num is palindrome: " + original);
+		} else {
+			System.out.println("num is not palindrome: " + rev);
+		}
+	}
+}
