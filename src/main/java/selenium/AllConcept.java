8485c6e11ebcb128455f0c03de26b2d738232a6a
masumrazait <masumrazait@gmail.com>
2025-09-25 23:01:51 +0530

    modification of code in selenium

--- src/main/java/selenium/AllConcept.java
+++ src/main/java/selenium/AllConcept.java
@@ -1,18 +1,24 @@
 package selenium;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class AllConcept {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -47,6 +53,15 @@ public class AllConcept {
 					action.moveToElement(listOfTools).perform();
 				}
 
+				File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				FileUtils.copyFile(screen, new File("./Images/page.png"));
+				driver.findElement(By.xpath("//a[text()='Tips']")).click();
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				js.executeScript("window.scrollBy(0,5000)");
+				driver.findElement(By.xpath("//button/*[local-name()='svg' and @class=\"lightToggleIcon_pyhR\"]"))
+						.click();
+				File ss = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				FileUtils.copyFile(ss, new File("./Images/iamges.png"));
 			}
 		}
 
