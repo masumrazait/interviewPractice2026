f29336247f045dd0b360f190b988fb6b22999d08
masumrazait <masumrazait@gmail.com>
2025-09-25 21:48:27 +0530

    added new code for selenium

--- src/main/java/selenium/AllConcept.java
+++ src/main/java/selenium/AllConcept.java
@@ -1,7 +1,10 @@
 package selenium;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,10 +14,21 @@ public class AllConcept {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://masumrazait.netlify.app/");
-		driver.getPageSource();
-		String title = driver.getTitle();
-		System.out.println(title);
+		String user = "admin";
+		String password = "admin";
+		String url = "https://" + user + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";
+		driver.get(url);
+		driver.findElement(By.xpath("//a[@href=\"http://elementalselenium.com/\"]")).click();
+		String parent = driver.getWindowHandle();
+		Set<String> Allwindow = driver.getWindowHandles();
+		Iterator<String> Il = Allwindow.iterator();
+		while (Il.hasNext()) {
+			String childWindow = Il.next();
+			if (!parent.equals(childWindow)) {
+				driver.switchTo().window(childWindow);
+				System.out.println("Child window title: " + driver.getTitle());
+			}
+		}
 		driver.quit();
 	}
 }
