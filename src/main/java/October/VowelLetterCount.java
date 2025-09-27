ed283f2e6a32d54bfaab5f1eb59a2406b854cf57
masumrazait <masumrazait@gmail.com>
2025-09-28 00:05:58 +0530

    added new code

--- src/main/java/October/VowelLetterCount.java
+++ src/main/java/October/VowelLetterCount.java
@@ -1,7 +1,14 @@
 package October;
 
 public class VowelLetterCount {
-public static void main(String[] args) {
-
-}
+	public static void main(String[] args) {
+		String str = "masumumbrella";
+		String v = "AEIOUaeiou";
+		System.out.print("vowel leter is: ");
+		for (char c : str.toCharArray()) {
+			if (v.indexOf(c) != -1) {
+				System.out.print( c+" ");
+			}
+		}
+	}
 }
