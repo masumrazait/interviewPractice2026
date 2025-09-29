45a2d625e8b4fee3fc358c67edf41f73ae0a2158
masumrazait <masumrazait@gmail.com>
2025-09-29 21:47:12 +0530

    added code

---
+++ src/main/java/October/CharacterCount.java
@@ -0,0 +1,16 @@
+package October;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterCount {
+	public static void main(String[] args) {
+		String str = "masum";
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (char c : str.toCharArray()) {
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+		System.out.println(map);
+	}
+
+}
