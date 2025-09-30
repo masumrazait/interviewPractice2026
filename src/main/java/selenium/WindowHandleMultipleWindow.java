7e195c5ed6d490fdb995bce3a50432c3398a6c91
masumrazait <masumrazait@gmail.com>
2025-09-30 22:25:48 +0530

    broken link

--- src/main/java/selenium/WindowHandleMultipleWindow.java
+++ src/main/java/selenium/WindowHandleMultipleWindow.java
@@ -1,10 +1,14 @@
 package selenium;
 
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.hc.core5.http.io.HttpServerConnection;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,20 +27,39 @@ public class WindowHandleMultipleWindow {
 		Set<String> AllWindow = driver.getWindowHandles();
 		Iterator<String> Il = AllWindow.iterator();
 		while (Il.hasNext()) {
-			String child = Il.next();
-			if (!parentWindow.equalsIgnoreCase(child)) {
-				driver.switchTo().window(child);
+			String childWindow = Il.next();
+			if (!parentWindow.equals(childWindow)) {
+				driver.switchTo().window(childWindow);
 				System.out.println(driver.getTitle());
-				List<WebElement> link = driver.findElements(By.tagName("a"));
-				System.out.println(link.size());
-				for (WebElement links : link) {
-					String href = links.getAttribute("href");
-					System.out.println(href);
+				List<WebElement> list = driver.findElements(By.tagName("a"));
+				System.out.println(list.size());
+				for (WebElement link : list) {
+					String url = link.getAttribute("href");
+					System.out.println(url);
+					if (url == null || url.isEmpty() || url.startsWith("http")) {
+						System.out.println("skipp invalid link: " + url);
+						continue;
+					}
+					try {
+						HttpsURLConnection connect = (HttpsURLConnection) new URL(url).openConnection();
+						connect.setRequestMethod("HEAD");
+						connect.connect();
+						int resposeCode = connect.getResponseCode();
+						if (resposeCode >= 400) {
+							System.out.println(url + " is a broken link.");
+						} else {
+							System.out.println(url + " is a valid links.");
+						}
+					} catch (Exception e) {
+						System.out.println("Error checing links: " + url + " -> " + e.getMessage());
+					}
 				}
+
 				driver.close();
 			}
 		}
 		driver.switchTo().window(parentWindow);
+		System.out.println("after returen main window: " + driver.getTitle());
 		driver.quit();
 	}
 }
