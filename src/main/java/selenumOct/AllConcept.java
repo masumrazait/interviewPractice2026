6e2c2578750f5fe8dc5f126929981b4d319d3849
masumrazait <masumrazait@gmail.com>
2025-10-07 21:12:56 +0530

    added new testcase

--- src/main/java/selenumOct/AllConcept.java
+++ src/main/java/selenumOct/AllConcept.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AllConcept {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -51,6 +51,22 @@ public class AllConcept {
 		System.out.println(cellValue);
 
 		driver.switchTo().window(parentWindow);
+		driver.get("https://www.flipkart.com/");
+
+		driver.findElement(By.name("q")).sendKeys("macbook pro");
+		Thread.sleep(4000);
+
+		List<WebElement> suggList = driver.findElements(By.xpath("//a[@class='oleBil']/div[2]"));
+		System.out.println(suggList.size());
+
+		for (int i = 0; i < suggList.size(); i++) {
+			String text = suggList.get(i).getText();
+			System.out.println(text);
+			if (text.contains("m4 max")) {
+				suggList.get(i).click();
+				break;
+			}
+		}
 		driver.quit();
 	}
 }
