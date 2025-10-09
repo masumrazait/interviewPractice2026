aaea1262db63089eb5002b6563145fb627fb760c
masumrazait <masumrazait@gmail.com>
2025-10-09 23:19:22 +0530

    added new code

---
+++ src/main/java/InterfacesConcept/Constructors.java
@@ -0,0 +1,14 @@
+package InterfacesConcept;
+
+class Bike {
+	Bike() {
+		System.out.println("running bike");
+	}
+}
+
+public class Constructors {
+	public static void main(String[] args) {
+		Bike b = new Bike();
+		System.out.println(b);
+	}
+}
