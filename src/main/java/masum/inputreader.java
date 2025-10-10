289d5018103c7d92010c46c828632777885430f1
masumrazait <masumrazait@gmail.com>
2025-10-10 22:37:03 +0530

    added new code

---
+++ src/main/java/masum/inputreader.java
@@ -0,0 +1,18 @@
+package masum;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class inputreader {
+	public static void main(String[] args) throws IOException {
+		String str = "./TestData/testdata.xlsx";
+		FileInputStream fis = new FileInputStream(str);
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		String cellValuleString = sheet.getRow(0).getCell(0).getStringCellValue();
+		System.out.println(cellValuleString);
+	}
+}
