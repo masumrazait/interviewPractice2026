0aceff69c5197e381a494b049c48a419d72c4ef7
masumrazait <masumrazait@gmail.com>
2025-10-19 19:21:42 +0530

    added new code

--- src/main/java/masum/NonRepeatingCharacter.java
+++ src/main/java/masum/NonRepeatingCharacter.java
@@ -4,8 +4,14 @@ public class NonRepeatingCharacter {
 	public static void main(String[] args) {
 		String str = "masumraza";
 		for (char c : str.toCharArray()) {
-			if (str.indexOf(c) == str.lastIndexOf(c)) {
-				System.out.println(c);
+			if (str.indexOf(c) != (str.lastIndexOf(c))) {
+				System.out.println("reapting character: " + c);
+			}
+		}
+		System.out.println(" ***************************************** ");
+		for (char d : str.toCharArray()) {
+			if (str.indexOf(d) == (str.lastIndexOf(d))) {
+				System.out.println("non repeating charecter: " + d);
 			}
 		}
 	}
