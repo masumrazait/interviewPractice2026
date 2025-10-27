29342154bec0470d4dc2283f72356e485ff672f3
masumrazait <masumrazait@gmail.com>
2025-10-27 23:26:28 +0530

    added new code in selenium for november and deleted old code

--- src/main/java/SeleniumNov/AlertSeleniumConcept.java
+++ src/main/java/SeleniumNov/AlertSeleniumConcept.java
@@ -1,14 +1,19 @@
 package SeleniumNov;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AlertSeleniumConcept {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -16,9 +21,10 @@ public class AlertSeleniumConcept {
 		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
 		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
 		Alert alert = driver.switchTo().alert();
-		Thread.sleep(3000);
-		System.out.println(alert.getText());
 		alert.accept();
+		Thread.sleep(3000);
+		File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screen, new File("./Images/image.png"));
 		driver.quit();
 	}
 }
