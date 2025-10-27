f070c3fa746f3f6789b5a38f875569c91d5c9b3a
masumrazait <masumrazait@gmail.com>
2025-10-27 23:16:34 +0530

    added new code

---
+++ src/main/java/SeleniumNov/AlertSeleniumConcept.java
@@ -0,0 +1,24 @@
+package SeleniumNov;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertSeleniumConcept {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		Alert alert = driver.switchTo().alert();
+		Thread.sleep(3000);
+		System.out.println(alert.getText());
+		alert.accept();
+		driver.quit();
+	}
+}
