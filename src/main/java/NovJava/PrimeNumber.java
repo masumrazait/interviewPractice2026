3ad5787e6e9f2bcbaef77e7ce43747cde776fc5d
masumrazait <masumrazait@gmail.com>
2025-10-28 23:59:17 +0530

    code change

--- src/main/java/NovJava/PrimeNumber.java
+++ src/main/java/NovJava/PrimeNumber.java
@@ -9,7 +9,7 @@ public class PrimeNumber {
 	}
 
 	private static boolean isPrime(int num) {
-		if (num < 1)
+		if (num <=1)
 			return false;
 		for (int i = 2; i <= Math.sqrt(num); i++) {
 			if (num % i == 0)
