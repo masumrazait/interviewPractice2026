14f9d63366e7e64c7ae9b16987929044099a4cd9
masumrazait <masumrazait@gmail.com>
2025-10-28 23:44:45 +0530

    Added new code

--- src/main/java/NovJava/WordCount.java
+++ src/main/java/NovJava/WordCount.java
@@ -6,12 +6,12 @@ import java.util.Map;
 public class WordCount {
 	public static void main(String[] args) {
 		String input = "raza patna blr ptn amn dell hp ms cg cts tcs tcs ms raza patna blr ptn";
-		String word[]=input.split(" ");
-		Map<String, Integer>wordCount=new HashMap<String, Integer>();
-		for(String words:word) {
-			wordCount.put(words, wordCount.getOrDefault(words, 0)+1);
+		String word[] = input.split(" ");
+		Map<String, Integer> wordCount = new HashMap<String, Integer>();
+		for (String words : word) {
+			wordCount.put(words, wordCount.getOrDefault(words, 0) + 1);
 		}
-		for(String words:wordCount.keySet()) {
+		for (String words : wordCount.keySet()) {
 			System.out.println(words + " = " + wordCount.get(words));
 		}
 	}
