aae0a208f0e869d944c27073720d0cdd1af57249
masumrazait <masumrazait@gmail.com>
2025-10-31 21:07:33 +0530

    Added new code for nov

--- src/main/java/masum/Test.java
+++ src/main/java/masum/Test.java
@@ -1,5 +1,7 @@
 package masum;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -17,14 +19,37 @@ public class Test {
 		driver.get("https://automationexercise.com/");
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,3000)");
-		WebElement src=driver.findElement(By.xpath("(//div[@class=\"single-products\"])[1]"));
-		Actions action =new Actions(driver);
+		WebElement src = driver.findElement(By.xpath("(//div[@class=\"single-products\"])[1]"));
+		Actions action = new Actions(driver);
 		action.moveToElement(src).perform();
 		driver.findElement(By.xpath("(//div[@class=\"product-overlay\"]//a[text()='Add to cart'])[1]")).click();
-		WebDriverWait wait=new WebDriverWait(driver,30);
+		WebDriverWait wait = new WebDriverWait(driver, 30);
 		wait.until(ExpectedConditions.titleContains("Automation Exercise"));
 		Thread.sleep(5000);
 		System.out.println(driver.getTitle());
+		driver.get("https://money.rediff.com/losers");
+		WebElement table = driver.findElement(By.xpath("//table[contains(@class,'dataTable')].//thead/tr/th"));
+		
+		// Get all header columns
+		List<WebElement> headers = table.findElements(By.xpath(".//thead/tr/th"));
+		System.out.println("==== TABLE HEADERS ====");
+		for (WebElement header : headers) {
+			System.out.print(header.getText() + "  |  ");
+		}
+		System.out.println("\n=========================");
+
+		// Get all rows in table body
+		List<WebElement> rows = table.findElements(By.xpath(".//tbody/tr"));
+
+		System.out.println("==== TABLE DATA ====");
+		for (WebElement row : rows) {
+			List<WebElement> cols = row.findElements(By.tagName("td"));
+			for (WebElement col : cols) {
+				System.out.print(col.getText() + "  |  ");
+			}
+			System.out.println();
+		}
+
 		driver.quit();
 	}
 
