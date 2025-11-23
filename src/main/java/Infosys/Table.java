022a5a990a21f78690d49f6a2cf12f24b8fcb9fd
masumrazait <masumrazait@gmail.com>
2025-11-23 22:22:10 +0530

    Added new code for java

--- src/main/java/Infosys/Table.java
+++ src/main/java/Infosys/Table.java
@@ -1,5 +1,15 @@
 package Infosys;
 
-public class Table {
+import java.util.Scanner;
 
+public class Table {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int num = sc.nextInt();
+		System.out.println("\n***** Table of " + num + " *****");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + " X " + i + " = " + num * i);
+		}
+	}
 }
