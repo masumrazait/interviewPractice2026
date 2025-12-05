dae978e44c7de903d769d9206cc1ecf07a38b0f2
masumrazait <masumrazait@gmail.com>
2025-12-05 22:04:34 +0530

    added new code for Accenture

---
+++ src/main/java/Accenture/Table.java
@@ -0,0 +1,16 @@
+package Accenture;
+
+import java.util.Scanner;
+
+public class Table {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Num : ");
+		int num = sc.nextInt();
+		System.out.println("*************** Table of " + num + " ***********");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + " X " + i + " = " + num * i);
+		}
+	}
+}
