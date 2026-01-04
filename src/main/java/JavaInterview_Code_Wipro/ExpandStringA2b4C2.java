347e853dfcf2fa28474be076d2c1a2b956529280
masumrazait <masumrazait@gmail.com>
2026-01-04 15:01:15 +0530

    Added new code

--- src/main/java/JavaInterview_Code_Wipro/ExpandStringA2b4C2.java
+++ src/main/java/JavaInterview_Code_Wipro/ExpandStringA2b4C2.java
@@ -1,14 +1,14 @@
 package JavaInterview_Code_Wipro;
 
-public class ExpandString {
+public class ExpandStringA2b4C2 {
 	public static void main(String[] args) {
-		String str = "a4b3c4";
+		String str = "a3b5c3";
 		String result = "";
 		for (int i = 0; i < str.length(); i += 2) {
-			char ch = str.charAt(i);
+			char c = str.charAt(i);
 			int count = Character.getNumericValue(str.charAt(i + 1));
 			for (int j = 1; j <= count; j++) {
-				result = result + ch;
+				result = result + c;
 			}
 		}
 		System.out.println(result);
