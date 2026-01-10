8b8cf1f2e8744912dda0fd755c3b3e3016908b8c
masumrazait <masumrazait@gmail.com>
2026-01-10 15:30:36 +0530

    Added code

---
+++ src/main/java/Coforge/StringCompresss.java
@@ -0,0 +1,20 @@
+package Coforge;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class StringCompresss {
+	public static void main(String[] args) {
+		String str = "aaavssssffff";
+		String result = "";
+		int count = 1;
+		Map<Character, Integer> map = new LinkedHashMap<>();
+		for (char c : str.toCharArray()) {
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+			result = result + entry.getKey() + entry.getValue();
+		}
+		System.out.println(result);
+	}
+}
