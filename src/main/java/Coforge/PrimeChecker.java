4af122d1aa8443381a17bf6ebeab27275b12e46c
masumrazait <masumrazait@gmail.com>
2026-01-10 16:44:45 +0530

    masum codded aadeed

---
+++ src/main/java/Coforge/PrimeChecker.java
@@ -0,0 +1,19 @@
+package Coforge;
+
+public class PrimeChecker {
+	public static void main(String[] args) {
+		for (int num = 1; num <= 100; num++) {
+			System.out.println(num + " is " + (isPrime(num) ? " prime number" : "not prime num"));
+		}
+	}
+
+	private static boolean isPrime(int num) {
+		if (num <= 2)
+			return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+}
