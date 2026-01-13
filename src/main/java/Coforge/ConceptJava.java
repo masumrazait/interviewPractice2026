bcd3bf54b95232b70e58000656a7551b795d4ee0
masumrazait <masumrazait@gmail.com>
2026-01-13 09:51:02 +0530

    Added new code

--- src/main/java/Coforge/ConceptJava.java
+++ src/main/java/Coforge/ConceptJava.java
@@ -1,5 +1,7 @@
 package Coforge;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -16,19 +18,33 @@ public class ConceptJava {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		String username = "admin";
 		String password = "admin";
-		String url = "https://" + username + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";
-		driver.get(url);
+		String urls = "https://" + username + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";
+		driver.get(urls);
 		Thread.sleep(4000);
 		System.out.println(driver.getTitle());
 		driver.get("https://masumrazait.netlify.app/");
 		List<WebElement> links = driver.findElements(By.tagName("a"));
 		System.out.println(links.size());
 		for (WebElement link : links) {
-			String href = link.getAttribute("href");
-			String text = link.getText();
-			System.out.println(href);
-			System.out.println(text);
+			String url = link.getAttribute("href");
+			if (url == null || url.isEmpty() || url.startsWith("javascript")) {
+				continue;
+			}
+			try {
+				HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+				connection.setConnectTimeout(3000);
+				connection.connect();
+				int responseCode = connection.getResponseCode();
+				if (responseCode >= 400) {
+					System.out.println("broken links: " + url + " -----> " + responseCode);
+				} else {
+					System.out.println("valid links: " + url + " -----> " + responseCode);
+				}
+			} catch (Exception e) {
+				System.out.println("Exception for URL: " + url);
+			}
 		}
+
 		driver.quit();
 	}
 }
