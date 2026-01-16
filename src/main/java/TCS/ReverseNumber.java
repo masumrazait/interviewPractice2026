8cde28c855a7cac15b79f53085f3e8e29446a6de
masumrazait <masumrazait@gmail.com>
2026-01-16 21:56:16 +0530

    added ReverseNumber

---
+++ src/main/java/TCS/ReverseNumber.java
@@ -0,0 +1,13 @@
+package TCS;
+
+public class ReverseNumber {
+	public static void main(String[] args) {
+		int num = 1234553;
+		int rev = 0;
+		while (num != 0) {
+			rev = rev * 10 + num % 10;
+			num = num / 10;
+		}
+		System.out.println(rev);
+	}
+}
