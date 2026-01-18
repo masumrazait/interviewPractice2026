58c7373265c289e1ad850b6cb605721eb13bebb7
masumrazait <masumrazait@gmail.com>
2026-01-18 19:38:21 +0530

    Added new code FindTheDuplicateNumber

---
+++ src/main/java/TCSMR/FindTheDuplicateNumber.java
@@ -0,0 +1,29 @@
+package TCSMR;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class FindTheDuplicateNumber {
+	public static void main(String[] args) {
+		int ar[] = { 133, 2, 134, 53554, 747, 747, 43634, 747, 86, 346 };
+		String str = "simaranmasum";
+		Set<Character> uniqchar = new LinkedHashSet<Character>();
+		Set<Character> dubchar = new LinkedHashSet<Character>();
+		Set<Integer> uniqNum = new LinkedHashSet<Integer>();
+		Set<Integer> dubNum = new LinkedHashSet<Integer>();
+		for (char c : str.toCharArray()) {
+			if (!uniqchar.add(c)) {
+				dubchar.add(c);
+			}
+		}
+		for (int num : ar) {
+			if (!uniqNum.add(num)) {
+				dubNum.add(num);
+			}
+		}
+		System.out.println(uniqchar);
+		System.out.println(uniqNum);
+	}
+
+}
