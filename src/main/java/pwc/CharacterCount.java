f7b882544d564940f1642d4a040a070cc90ad14e
masumrazait <masumrazait@gmail.com>
2026-01-20 21:49:21 +0530

    added CharacterCount

--- src/main/java/pwc/CharacterCount.java
+++ src/main/java/pwc/CharacterCount.java
@@ -2,6 +2,19 @@ package pwc;
 
 public class CharacterCount {
 	public static void main(String[] args) {
+		int num = 9999999;
+		while (num > 10) {
+			num = sumDigits(num);
+		}
+		System.out.println("SingleDgit sum: " + num);
+	}
 
+	public static int sumDigits(int n) {
+		int sum = 0;
+		while (n > 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+		return sum;
 	}
 }
