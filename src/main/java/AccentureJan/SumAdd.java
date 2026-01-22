7be92e2270b30e9b84699803c083beb706855494
masumrazait <masumrazait@gmail.com>
2026-01-22 23:14:26 +0530

    added new code SumAdd

---
+++ src/main/java/AccentureJan/SumAdd.java
@@ -0,0 +1,16 @@
+package AccentureJan;
+
+public class SumAdd {
+	public static void main(String[] args) {
+		int num = 134567;
+		while (num >= 10) {
+			int sum = 0;
+			while (num > 0) {
+				sum += num % 10;
+				num = num / 10;
+			}
+			num = sum;
+		}
+        System.out.println("Single digit sum = " + num);
+	}
+}
