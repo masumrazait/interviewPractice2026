cefc24b9894ddc2fb0b56f7eecf813a10197874a
masumrazait <masumrazait@gmail.com>
2026-01-25 00:01:43 +0530

    added new code

---
+++ src/main/java/january/WebTable.java
@@ -0,0 +1,32 @@
+package january;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTable {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,5000)");
+		
+		int Rowcount = driver.findElements(By.xpath("//table[@id=\"customers\"]/tbody//tr")).size();
+		int Colcount = driver.findElements(By.xpath("//table[@id=\"customers\"]/tbody//th")).size();
+
+		for (int i = 1; i < Rowcount; i++) {
+			for (int j = 1; j <= Colcount; j++) {
+				String act = driver.findElement(By.xpath("//table[@id=\"customers\"]/tbody/tr[" + (i + 1) + "]/td[" + j + "]")).getText();
+			}
+		}
+
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+}
