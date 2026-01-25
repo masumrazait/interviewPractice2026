0aefc27cdf5b45de4d21bce0fbf12527747b57bf
masumrazait <masumrazait@gmail.com>
2026-01-25 20:50:44 +0530

    added new code Reverse

---
+++ src/main/java/january/Reseve.java
@@ -0,0 +1,10 @@
+package january;
+
+public class Reseve {
+	public static void main(String[] args) {
+		int arr[] = { 1, 22, 33, 44, 55, 66, 77, 42, 32, 343, 54, 64 };
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
