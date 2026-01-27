256f59cfaa47eb80caba9e232ddc8e0f3baae13a
masumrazait <masumrazait@gmail.com>
2026-01-28 00:27:25 +0530

    Added new code

--- src/main/java/TCS/SeleniumJava.java
+++ src/main/java/TCS/SeleniumJava.java
@@ -6,17 +6,20 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 public class SeleniumJava {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		ChromeOptions opp = new ChromeOptions();
 		opp.addArguments("--headless");
-		WebDriver driver = new ChromeDriver(opp);
+//		WebDriver driver = new ChromeDriver(opp);
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://interviewlikeapro.blogspot.com/2026/01/complete-rest-assured-interview.html");
@@ -53,7 +56,26 @@ public class SeleniumJava {
 
 		driver.get("https://the-internet.herokuapp.com/");
 		driver.findElement(By.xpath("//a[text()='A/B Testing']")).click();
+		driver.get("https://testautomationpractice.blogspot.com/");
 
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("document.getElementById('name').value='masum';");
+
+		Actions action = new Actions(driver);
+		List<WebElement> days = driver.findElements(By.xpath("//label[@for=\"days\"]/following-sibling::div/label"));
+		System.out.println(days.size());
+		for (WebElement day : days) {
+			String week = day.getAttribute("for");
+			System.out.println(week);
+			action.moveToElement(day);
+			Thread.sleep(300);
+			if (day.getText().equalsIgnoreCase("saturday")) {
+				day.click();
+				System.out.println(week);
+			}
+		}
+
+		Thread.sleep(3000);
 		driver.quit();
 	}
 }
