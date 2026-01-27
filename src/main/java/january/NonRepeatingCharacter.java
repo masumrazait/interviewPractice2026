45a7542d5482c3a0c28acb11ef9ad797faf23d50
masumrazait <masumrazait@gmail.com>
2026-01-27 22:55:02 +0530

    added new code

---
+++ src/main/java/january/NonRepeatingCharacter.java
@@ -0,0 +1,21 @@
+package january;
+
+public class NonRepeatingCharacter {
+	public static void main(String[] args) {
+		String str = "patnabiharamnoursarandelhi";
+		for (char c : str.toCharArray()) {
+			if (str.indexOf(c) == str.lastIndexOf(c)) {
+				System.out.print(c + " ");
+			}
+		}
+		System.out.println("repeat : ");
+		for (char d : str.toCharArray()) {
+			String temp = "";
+			if (str.indexOf(d) != str.lastIndexOf(d)) {
+				temp += str;
+				System.out.print(d + " ");
+			}
+		}
+	}
+
+}
