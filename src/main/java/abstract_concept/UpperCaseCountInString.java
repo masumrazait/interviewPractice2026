2a389dcc080a43c0a0a8741ed22b33466e06355d
masumrazait <masumrazait@gmail.com>
2026-01-29 00:25:13 +0530

    Added new code

---
+++ src/main/java/abstract_concept/UpperCaseCountInString.java
@@ -0,0 +1,13 @@
+package abstract_concept;
+
+public class UpperCaseCountInString {
+	public static void main(String[] args) {
+		String str = "MaDhUBani";
+		int count = 0;
+		for (char c : str.toCharArray()) {
+			if (Character.isUpperCase(c)) {
+				System.out.print(c + " ");
+			}
+		}
+	}
+}
