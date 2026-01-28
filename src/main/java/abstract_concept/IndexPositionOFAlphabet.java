d2dbed27e75a721195a199588bd90edc77a65c04
masumrazait <masumrazait@gmail.com>
2026-01-28 23:18:54 +0530

    added new code

--- src/main/java/abstract_concept/IndexPositionOFAlphabet.java
+++ src/main/java/abstract_concept/IndexPositionOFAlphabet.java
@@ -2,11 +2,14 @@ package abstract_concept;
 
 public class IndexPositionOFAlphabet {
 	public static void main(String[] args) {
-		String str="IndexPositionOFAlphabet";
-		char index='m';
-		System.out.print("Index of "+index+" are: ");
-		for(int i=0;i<=str.length()-1;i++) {
-			
+		String str = "dsIndexPositionOFAlphabet".toLowerCase();
+		char index = 'i';
+		// int indexPosition = str.indexOf(index);
+		System.out.print("Index of " + index + " are: ");
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == index) {
+				System.out.print(i + " ");
+			}
 		}
 	}
 }
