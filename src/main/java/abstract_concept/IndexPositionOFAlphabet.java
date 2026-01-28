a719531defbfa21b092ff82c028ffb58856923e8
masumrazait <masumrazait@gmail.com>
2026-01-28 22:37:07 +0530

    added new code

---
+++ src/main/java/abstract_concept/IndexPositionOFAlphabet.java
@@ -0,0 +1,12 @@
+package abstract_concept;
+
+public class IndexPositionOFAlphabet {
+	public static void main(String[] args) {
+		String str="IndexPositionOFAlphabet";
+		char index='m';
+		System.out.print("Index of "+index+" are: ");
+		for(int i=0;i<=str.length()-1;i++) {
+			
+		}
+	}
+}
