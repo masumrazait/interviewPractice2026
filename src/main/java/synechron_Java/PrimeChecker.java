23d299b2ae47475868c37783a5d38965b0130065
masumrazait <masumrazait@gmail.com>
2026-01-31 19:44:02 +0530

    added new code PrimeChecker

---
+++ src/main/java/synechron_Java/PrimeChecker.java
@@ -0,0 +1,20 @@
+package synechron_Java;
+
+public class PrimeChecker {
+	public static void main(String[] args) {
+		for (int num = 1; num <= 100; num++) {
+			System.out.println(num + " is " + (isPrime(num) ? "prime number" : "not prime number"));
+		}
+	}
+
+	private static boolean isPrime(int num) {
+		if (num <= 1)
+			return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+}
