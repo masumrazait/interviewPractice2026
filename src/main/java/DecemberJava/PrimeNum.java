07e6e7716007b35296ea06539d527a64feda27fd
masumrazait <masumrazait@gmail.com>
2026-02-03 00:31:47 +0530

    added new code PrintUniqCharacter

---
+++ src/main/java/DecemberJava/PrimeNum.java
@@ -0,0 +1,19 @@
+package DecemberJava;
+
+public class PrimeNum {
+	public static void main(String[] args) {
+		for (int num = 1; num <= 100; num++) {
+			System.out.println(num + " is " + (isPrime(num) ? "prime" : "not prime num"));
+		}
+	}
+
+	private static boolean isPrime(int num) {
+		if (num <= 1)
+			return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+}
