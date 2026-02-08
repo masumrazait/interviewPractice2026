552bad508f832af7587e38016af670beda0e3fbb
masumrazait <masumrazait@gmail.com>
2026-02-08 23:41:41 +0530

    Merge branch 'Raza' of https://github.com/masumrazait/interviewPractice2026 into Raza
    # Conflicts:
    #	target/classes/META-INF/maven/com.interviewPractice2026/interviewPractice2026/pom.properties
    #	target/classes/Tests/BasantpurBangla.class
    #	target/classes/Tests/BrokenLink.class
    #	target/classes/januarySeleniumWipro/Hellow.class

--- src/main/java/aaASelenium/GoogleLangLinks.java
+++ src/main/java/aaASelenium/GoogleLangLinks.java
@@ -1,5 +1,7 @@
 package aaASelenium;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -7,23 +9,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleLangLinks {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(opt);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("https://www.google.com/");
+		driver.get("https://www.flipkart.com/");
 		driver.findElements(By.tagName("a"));
 		List<WebElement> links = driver.findElements(By.tagName("a"));
 		System.out.println(links.size());
 		for (WebElement link : links) {
-			String href = link.getAttribute("href");
-			System.out.println(href);
+			String url = link.getAttribute("href");
+			if (url == null || url.isEmpty() || url.startsWith("javascript")) {
+				continue;
+			}
+			try {
+				HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+				con.setConnectTimeout(50);
+				con.connect();
+				int responseCode = con.getResponseCode();
+				if (responseCode >= 400) {
+					System.out.println("❌ Broken Link: " + url + " --> " + responseCode);
+				} else {
+					System.out.println("✅ Valid Link: " + url + " --> " + responseCode);
+				}
+
+			} catch (Exception e) {
+				System.out.println("⚠️ Exception for URL: " + url);
+			}
 		}
 		System.out.println(driver.getTitle());
 		driver.quit();
 	}
-
 }
