8e1af376a7e9673c9c530bd3b4db3331ac888e02
masumrazait <masumrazait@gmail.com>
2026-02-10 22:17:27 +0530

    Added new code DublicateWord

---
+++ src/main/java/Walmart/UniqCharat.java
@@ -0,0 +1,18 @@
+package Walmart;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class UniqCharat {
+	public static void main(String[] args) {
+		String str = "masum raza";
+		Set<Character> uniq = new LinkedHashSet<Character>();
+		Set<Character> dub = new LinkedHashSet<Character>();
+		for (char c : str.toCharArray()) {
+			if (!uniq.add(c)&& c!=' ') {
+				dub.add(c);
+			}
+		}
+		System.out.println(uniq);
+	}
+}
