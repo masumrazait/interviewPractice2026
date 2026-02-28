d9e9571a5bd26079bfba9492b9631873797c67ec
masumrazait <masumrazait@gmail.com>
2026-02-28 22:28:02 +0530

    added new code

--- src/test/java/March/Selenium.java
+++ src/test/java/March/Selenium.java
@@ -1,26 +1,31 @@
 package March;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class Selenium {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		String user = "admin";
 		String pswd = "admin";
-		String url = "https://" + user + ":" + pswd + "@" + "the-internet.herokuapp.com/basic_auth";
-		driver.get(url);
+		String appUrl = "https://" + user + ":" + pswd + "@" + "the-internet.herokuapp.com/basic_auth";
+		driver.get(appUrl);
 		System.out.println(driver.getTitle());
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//a[text()='Elemental Selenium']")).click();
@@ -40,8 +45,16 @@ public class Selenium {
 					Actions action = new Actions(driver);
 					action.moveToElement(getHeader).perform();
 					System.out.println(getHeader.getText());
-
+					continue;
 				}
+				List<WebElement> links = driver.findElements(By.tagName("a"));
+				System.out.println(links.size());
+				for (WebElement link : links) {
+					String url = link.getAttribute("href");
+					System.out.println(url);
+				}
+				File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				FileUtils.copyFile(screen, new File("./Images/img.png"));
 				driver.close();
 			}
 		}
